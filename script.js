de6332ca32b2870a1ea9e0c2f1760ada53df0a45
Sean <zimbiekitten@gmail.com>
2021-05-10 16:54:04 -0700

    jquery stuff

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-function Time(time, limit = 'min') {
-    let milliseconds = parseInt((time % 1000)),
-        seconds = Math.floor((time / 1000) % 60),
-        minutes = Math.floor((time / (1000 * 60)) % 60),
-        hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+`use strict`
 
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
+$(function () {
+    function Time(time, limit = 'min') {
+        let milliseconds = parseInt((time % 1000)),
+            seconds = Math.floor((time / 1000) % 60),
+            minutes = Math.floor((time / (1000 * 60)) % 60),
+            hours = Math.floor((time / (1000 * 60 * 60)) % 24);
 
-    if (limit == 'sec') {
-        return hours + ':' + minutes + ':' + seconds;
-    } else if (limit == 'min') {
-        return hours + ':' + minutes;
-    } else if (limit == 'hour') {
-        return hours;
-    } else {
-        return hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        if (limit == 'sec') {
+            return hours + ':' + minutes + ':' + seconds;
+        } else if (limit == 'min') {
+            return hours + ':' + minutes;
+        } else if (limit == 'hour') {
+            return hours;
+        } else {
+            return hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
+        }
     }
-}
 
-fetch('./weather.json')
-    .then(response => response.json())
-    .then(data => init(data));
+    $( "#hourly" ).click(change)
 
-function init(json) {
-    let { lat, lon, current, daily, hourly, ...otherObj } = json
-    console.log(current);
-    console.log(Time(current['sunrise']))
-    console.log(Time(current['sunset']))
-    console.log(Time(current['dt']))
-}
+    function change() {
+        let id = this.id
+        console.log(id);
+    }
+
+    fetch('./weather.json')
+        .then(response => response.json())
+        .then(data => init(data));
+
+    function init(json) {
+        let { lat, lon, current, daily, hourly, ...otherObj } = json
+        console.log(current);
+        console.log(Time(current['sunrise']))
+        console.log(Time(current['sunset']))
+        console.log(Time(current['dt']))
+    }
+});
